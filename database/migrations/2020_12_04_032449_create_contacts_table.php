a99d8189c2532de8c5047470bc32056caef962b7
Muhamad Virgiawan Dwi Rangga <virgiawndwir@gmail.com>
2020-12-04 10:53:49 +0700

    first commit

---
+++ database/migrations/2020_12_04_032449_create_contacts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('protocol');
+            $table->string('smtp_user');
+            $table->string('smtp_host');
+            $table->string('smtp_pass');
+            $table->integer('smtp_port');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
